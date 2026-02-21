8f22639567f9aa52eefbbdbdb98d886ff19c8e2a
sundar-pt8033 <sundar-pt8033@SUNDAR-PT8033>
2026-02-21 23:09:52 +0530

    git merge vs rebase

--- gitTask1.java
+++ gitTask1.java
@@ -2,5 +2,6 @@ public class gitTask1 {
  public static void main(String[] args) {
     System.out.println("hi");
     System.out.println("branch 2 created");
+     System.out.println("git merge vs git rebase demo");
  }   
 }
